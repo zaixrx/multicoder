f5d451e9e6e15beb2e7e85c14e786fea0a899cdb
zaixrx <zaizroff@gmail.com>
2025-04-19 11:48:39 +0100

    Wake up server

--- back-end/index.ts
+++ back-end/index.ts
@@ -2,11 +2,16 @@ import { config } from "dotenv";
 import cors from "cors";
 import express from "express";
 import eventHandlers from "./eventHandlers/eventsHandlers";
+import { wakeUpServer } from "./wakeUpServer";
 const initializeServer = require("socket.io");
 
 config();
 const app = express();
 app.use(cors({ origin: "*" }));
+
+const baseURL = process.env.BASE_URL;
+if (!baseURL) throw new Error("BASE_URL is not defined in .env file");
+wakeUpServer(baseURL);
 
 const PORT = process.env.PORT;
 const server = app.listen(PORT, () => {
---
+++ back-end/wakeUpServer.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+// Used to not let the server shut down after 5 minutes.
+export function wakeUpServer(url: string) {
+  setInterval(async () => {
+    try {
+      await axios.get(url);
+      console.log("Server Wake Up");
+    } catch (error) {
+      console.error(error);
+    }
+  }, 1000);
+}
